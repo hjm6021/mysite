f3d3506b2c5f5e24d0824f26d192eb8ec66541f9
Han Jeongmin <hjm6021@gmail.com>
2021-04-29 21:08:15 +0900

    finish home page

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from mysite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomeView.as_view(), name="home")
+    path('', views.HomeView.as_view(), name="home"),
+    path('about/', views.AboutView.as_view(), name="about")
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -3,3 +3,6 @@ from django.views.generic.base import TemplateView
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "index.html"
+
+class AboutView(TemplateView):
+    template_name = "about.html"
