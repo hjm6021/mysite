eb995ab45288690ea479c2d1bcd42fe16dfd2f59
Han Jeongmin <hjm6021@gmail.com>
2021-05-04 02:20:37 +0900

    add ckeditor

--- investment/forms.py
+++ investment/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from .models import Post, Category
-from ckeditor.widgets import CKEditorWidget
+from ckeditor_uploader.widgets import CKEditorWidget, CKEditorUploadingWidget
 
 class PostForm(forms.Form):
     title = forms.CharField(widget=forms.Textarea(attrs={'cols': '80', 'rows': '1'}))
     category = forms.ModelChoiceField(queryset=Category.objects.all())
     description = forms.CharField(widget=forms.Textarea(attrs={'cols': '80', 'rows': '3'}))
-    content = forms.CharField(widget = CKEditorWidget())
+    content = forms.CharField(widget = CKEditorUploadingWidget())
