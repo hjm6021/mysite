e11be9709da88a3eeeac6a4913d664512465239f
Han Jeongmin <hjm6021@gmail.com>
2021-06-15 19:37:07 +0900

    check right to edit or delete

--- medicine/views.py
+++ medicine/views.py
@@ -6,7 +6,7 @@ from medicine.models import Medicine
 def index(request):
     # Get으로 페이지 번호 획득  
     template_name = "medicine/medicine_index.html"
-    dates = Medicine.objects.all().order_by("-medicine_dt")
+    dates = Medicine.objects.all().order_by("-medicine_dt")[:5]
     content = {'dates':dates}
     return render(request, template_name, content)
 
